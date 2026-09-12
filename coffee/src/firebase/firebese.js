7f8bc6fc06c4dd803d36ad43947cb3620200e966
Mohammad Al Bzoor <mohammedbzoor777@gmail.com>
2026-09-12 20:14:39 +0300

    security: move Firebase config to environment variables
    - Replace hardcoded API keys in firebese.js with process.env variables
    - Add .env to .gitignore to prevent secret exposure
    - Add .env.example as template for developers

--- coffee/src/firebase/firebese.js
+++ coffee/src/firebase/firebese.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCQgkQwc17ETzK5ToY8WfTDpGO3VVpfjiY",
-  authDomain: "teslacoffee-04.firebaseapp.com",
-  projectId: "teslacoffee-04",
-  storageBucket: "teslacoffee-04.firebasestorage.app",
-  messagingSenderId: "805172099232",
-  appId: "1:805172099232:web:bb84b3422c2241777eb1c5",
-  measurementId: "G-R5XJ945MRY"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
